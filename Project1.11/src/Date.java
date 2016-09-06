476c894f6eb0a6810a1afbf71eff6fd32602da81
nmd001 <nmd001@lyn188-07>
2016-09-06 12:22:20 -0400

    lab update

--- Project1.11/src/Date.java
+++ Project1.11/src/Date.java
@@ -1,5 +1,7 @@
+import java.util.*;
 
 public class Date {
+	
 	private int month, day, year;
 	
 	public Date(int m, int d, int y){
@@ -8,5 +10,16 @@ public class Date {
 		year = y;
 	}
 	
+	//Constructor makes a random date to solve for
+	//UPDATE: needs limit on days for months < 31 days
+	public Date(){
+		Random gen = new Random();
+		month = gen.nextInt(12) + 1;
+		day = gen.nextInt(31) + 1;
+		year = gen.nextInt(200);
+	}
 	
+	public int dayOfTheWeek(){
+		int leapYears = year / 4;
+	}
 }
