a372949670840cac1f7f7b76423e236836513750
nmd001 <nmd001@lyn188-07>
2016-11-10 12:49:14 -0500

    k

--- HW 8.1-5/src/IteratorHomework.java
+++ HW 8.1-5/src/IteratorHomework.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class IteratorHomework {
 
 	public static void main(String[] args) {
 		Scanner r = new Scanner(System.in);
